55197e274d3d0e1784bc91139fb81ff9cd98ad31
GurleenA <101472021+GurleenA@users.noreply.github.com>
2024-02-10 23:25:32 -0700

    fetching the questions

--- quiz_javascript.js
+++ quiz_javascript.js
@@ -2,5 +2,6 @@ async function getQuestion() {
     const response = await fetch("https://opentdb.com/api.php?amount=25&category=15&type=multiple");
     const questions = await response.json();
     console.log(questions);
+    question.innerHTML= questions.results[1].question;
 }getQuestion();
 
